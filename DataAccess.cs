3fca66f351c83b2b436f0d4f4872f6e602d4ca13
Umair-Faisal <127207706+Umair-Faisal@users.noreply.github.com>
2023-03-16 18:17:33 +0500

    Add files via upload

---
+++ DataAccess.cs
@@ -0,0 +1,134 @@
+﻿using System.ComponentModel;
+using DataProjectLib;
+
+namespace ConsoleAppUI
+{
+    public static class DataAccess
+    {
+
+        public static void      AddStudent()
+        {
+
+            Console.WriteLine("Enter Information of Student:");
+            Console.Write("Roll No. : ");
+            int RollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("First Name : ");
+            string FirstName = Console.ReadLine();
+            Console.Write("Last Name : ");
+            string LastName = Console.ReadLine();
+            Console.Write("CGPA : ");
+            decimal CGPA = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Session : ");
+            int Session = Convert.ToInt32(Console.ReadLine());
+
+            Student student = new Student(RollNo, FirstName, LastName, CGPA);
+
+            using (var context = new Context())
+            {
+                context.Students.Add(student);
+                var session = context.Sessions.Single(b => b.Year == Session);
+                session.Students.Add(student);
+                context.SaveChanges();
+            }
+        }
+
+        public static void      GetStudent(int roll)
+        {
+            try
+            {
+                Student uStudent = ReturnStudent(roll);
+                PrintInfo<Student>(uStudent);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("No Student Found");
+            }
+
+        }
+        public static void      UpdateStudent(int roll)
+        {
+            try
+            {
+                Student uStudent = ReturnStudent(roll);
+                PrintInfo<Student>(uStudent);
+                Console.WriteLine("Enter New Info Of Student");
+                Console.Write("First Name : ");
+                string FirstName = Console.ReadLine();
+                Console.Write("Last Name : ");
+                string LastName = Console.ReadLine();
+                Console.Write("CGPA : ");
+                decimal CGPA = Convert.ToDecimal(Console.ReadLine());
+                using (var context = new Context())
+                {
+                    Student stu = context.Students.Single(s => s.Rollno == roll);
+                    if(FirstName != null)stu.FirstName = FirstName;
+                    if (LastName != null) stu.LastName = LastName;
+                    stu.CGPA = CGPA;
+                    context.SaveChanges();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("No Student Found");
+            }
+        }
+        public static void      DeleteStudent(int roll)
+        {
+            try
+            {
+                using (var context = new Context())
+                {
+                    context.Students.Remove(ReturnStudent(roll));
+                    context.SaveChanges();
+                }
+                Console.WriteLine("Successfully Deleted the Student");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Student Not Found");
+            }
+         
+        }
+        public static void      Addteacher()
+        {
+            Console.WriteLine("Enter Information of Teacher: ");
+            Console.Write("Enter Teacher ID: ");
+            int ID = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Teacher First Name: ");
+            string FirstName = Console.ReadLine();
+            Console.Write("Enter Teacher Last Name: ");
+            string LastName = Console.ReadLine();
+
+            Teacher Teacher = new Teacher(ID, FirstName, LastName);
+
+            using (var context = new Context())
+            {
+                context.Teachers.Add(Teacher);
+                context.SaveChanges();
+            }
+
+        }
+        private static void     PrintInfo<T>(T obj)
+        {
+            Console.WriteLine("Description: ");
+            foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(obj))
+            {
+                string name = descriptor.Name;
+                object value = descriptor.GetValue(obj);
+                
+                Console.WriteLine("{0}  =   {1}", name, value);
+            }
+        }
+        public static Student   ReturnStudent(int ID)
+        {
+            var student = new Student();
+            using (var context = new Context())
+            {
+                student = context.Students.Single(b => b.Rollno == ID);
+            }
+            return student;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ConsoleAppUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Start1:
+            Console.WriteLine("Select A Number: ");
+            Console.WriteLine("1. Add Student \n2.Find Student\n3.Update Student\n4.Delete Student");
+            byte OP = 0;
+            try { OP = Convert.ToByte(Console.ReadLine()); } catch (FormatException) { }
+            Console.Clear();
+            switch (OP)
+            {
+                case 1:
+                    DataAccess.AddStudent();
+                    break;
+                case 2:
+                    Console.Write("Enter RollNo. of Student: ");
+                    int roll1 = Convert.ToInt32(Console.ReadLine());
+                    DataAccess.GetStudent(roll1);
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+                case 3:
+                    Console.Write("Enter RollNo. of Student: ");
+                    int roll2 = Convert.ToInt32(Console.ReadLine());
+                    DataAccess.UpdateStudent(roll2);
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+                case 4:
+                    Console.Write("Enter RollNo. of Student: ");
+                    int roll3 = Convert.ToInt32(Console.ReadLine());
+                    DataAccess.DeleteStudent(roll3);
+                    Console.ReadKey();
+                    break;
+                default:
+                    Console.WriteLine("Invalid Option");
+                    Console.ReadKey();
+                    break;
+            }
+            goto Start1;
+
+            //DataAccess.SearchStudent();
+            //DataAccess.AddStudent();
+            //DataAccess.Addteacher();
+            //DataAccess.UpdateStudent();
+        }
+
+            
+    }
+}
